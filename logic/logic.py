aee0aa669bc4523ea6fb9fa206a73842f2b9f10c
johan <joaramirezra@unal.edu.co>
2021-01-27 21:13:41 -0500

    v0.7 with dataframe managment

--- logic/logic.py
+++ logic/logic.py
@@ -1,2 +1,13 @@
-print('hello world')
+from datetime import datetime
+import pandas as pd
 
+from manejo_lineas import crear_linea_con_fecha
+from manejo_archivos import crear_archivos
+from Manejo_alimento import *
+from manejo_huevos import *
+
+
+crear_archivos()
+inicializar_inventario()
+obtener_inventario()
+agregar_al_registro(0,1,'3','2','5')
