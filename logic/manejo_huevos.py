087255d97f391fa6080d0edf1c506157bc563af5
johan <joaramirezra@unal.edu.co>
2021-02-06 23:27:01 -0500

    V1.5 adding GUI

--- logic/manejo_huevos.py
+++ logic/manejo_huevos.py
@@ -1,19 +1,22 @@
 from manejo_lineas import crear_linea_con_fecha
 import pandas as pd 
 from datetime import datetime
+
+
 tipo_de_gallina = ['POLLITA','GALLINA']
 tipos_huevo = ['PIPO','B','A','AA','AAA','JUMBO','BLANCO','VENCIDO_VENTA','DESTRUIDOS']
 presentacion_huevos = ['unidad','cubeta','sobrante']
+
 #------------------------------------------------------------------------------
 # Asociados al manejo de huevos
 def llenar_inicio_dia_inventario():
-	f = open("./files/ingreso_huevos.csv", "a")	
+	f = open("./logic/files/ingreso_huevos.csv", "a")	
 	for tipo in tipo_de_gallina:
 		for huevo in tipos_huevo:
 				f.write(crear_linea_con_fecha(tipo,huevo,'0','0','0')+'\n')
 
 def agregar_huevos(galpon,tipo,*valores):
-	file = open("./files/ingreso_huevos.csv",'a')
+	file = open("./logic/files/ingreso_huevos.csv",'a')
 	tipo_gallina = tipo_de_gallina[galpon]
 	tipo_huevo = tipos_huevo[tipo]
 	linea = crear_linea_con_fecha(tipo_gallina,tipo_huevo,*valores)
@@ -24,19 +27,19 @@ def agregar_huevos(galpon,tipo,*valores):
 		llenar_inicio_dia_inventario()
 
 def obtener_produccion():
-	df = pd.read_csv("./files/ingreso_huevos.csv",sep=';',index_col=False)
+	df = pd.read_csv("./logic/files/ingreso_huevos.csv",sep=';',index_col=False)
 	df = df.groupby('tipo').sum().reset_index()
 	return df
 
 def obtener_produccion_diaria():
-	df = pd.read_csv("./files/ingreso_huevos.csv",sep=';',index_col=False)
+	df = pd.read_csv("./logic/files/ingreso_huevos.csv",sep=';',index_col=False)
 	hoy = str(datetime.today().strftime('%d/%m/%Y'))
 	df = df[df['fecha'] == hoy]
 	
 	if (df['tipo'].shape[0] < 18 ):
 		llenar_inicio_dia_inventario()
 	
-	df = pd.read_csv("./files/ingreso_huevos.csv",sep=';',index_col=False)
+	df = pd.read_csv("./logic/files/ingreso_huevos.csv",sep=';',index_col=False)
 	hoy = str(datetime.today().strftime('%d/%m/%Y'))
 	df = df[df['fecha'] == hoy]
 	df = df.groupby(['galpon','tipo']).sum().reset_index()
